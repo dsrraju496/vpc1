53d2b60e8be415029fd3e90170fc04da04fd7592
Ubuntu <sitaramaraju0496@gmail.com>
2021-07-06 13:24:27 +0000

    secon commit

--- vpc.tf
+++ vpc.tf
@@ -1,10 +1,4 @@
 
-provider "aws" {
-   region = "us-west-2"
-  access_key = "AKIA3WOCLZMYWJT37WW7"
-  secret_key ="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"
-}
-
 resource "aws_vpc" "vpc" {
   cidr_block       = "10.0.0.0/16"
   instance_tenancy = "default"
